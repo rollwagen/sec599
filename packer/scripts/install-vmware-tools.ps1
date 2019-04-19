b238be0a989a96a53ba20a04e89a9fe2e24ee7ac
rollwagen@gmail.com <rollwagen@gmail.com>
2019-04-18 23:00:12 -0700

    Cleaned up old .bat remains

--- packer/scripts/install-vmware-tools.ps1
+++ packer/scripts/install-vmware-tools.ps1
@@ -17,25 +17,5 @@ $vmwareToolsSetupExe = $driveLetter + ':\' + "setup64.exe"
 cmd /c $vmwareToolsSetupExe /S /v"/qn REBOOT=R\"
 
 Write-Host "Cleaning up vmware tools install...."
-
-
-
-if exist "C:\Users\vagrant\windows.iso" (
-    move /Y C:\Users\vagrant\windows.iso C:\Windows\Temp
-)
-
-if not exist "C:\Windows\Temp\windows.iso" (
-    powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://softwareupdate.vmware.com/cds/vmw-desktop/ws/15.0.2/10952284/windows/packages/tools-windows.tar',
-    'C:\Windows\Temp\vmware-tools.tar')" <NUL
-    cmd /c ""C:\Program Files\7-Zip\7z.exe" x C:\Windows\Temp\vmware-tools.tar -oC:\Windows\Temp"
-    FOR /r "C:\Windows\Temp" %%a in (VMware-tools-windows-*.iso) DO REN "%%~a" "windows.iso"
-    rd /S /Q "C:\Program Files (x86)\VMWare"
-)
-
-cmd /c ""C:\Program Files\7-Zip\7z.exe" x "C:\Windows\Temp\windows.iso" -oC:\Windows\Temp\VMWare"
-cmd /c C:\Windows\Temp\VMWare\setup.exe /S /v"/qn REBOOT=R\"
-
-del /Q "C:\Windows\Temp\vmware-tools.tar"
-del /Q "C:\Windows\Temp\windows.iso"
-rd /S /Q "C:\Windows\Temp\VMware"
-goto :done
+Dismount-DiskImage -ImagePath C:\Windows\Temp\vmware-tools.iso
+Remove-Item  C:\Windows\Temp\vmware-tools.iso
