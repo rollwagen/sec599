5cebe496a0f0bcce3ab6b00836e9534f9c264f0e
rollwagen@gmail.com <rollwagen@gmail.com>
2019-04-18 23:23:40 -0700

    Fixed quotation / string escate for setup.exe call

--- packer/scripts/install-vmware-tools.ps1
+++ packer/scripts/install-vmware-tools.ps1
@@ -1,12 +1,9 @@
 
-
-Write-Host "Enabling >TLS1.0"
+Write-Host "Enabling >TLS1.0 for iso download over https..."
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 Write-Host "Downloading vmware tools..."
-#(New-Object System.Net.WebClient).DownloadFile('https://softwareupdate.vmware.com/cds/vmw-desktop/ws/15.0.4/12990004/windows/packages/tools-windows.tar', 'C:\Windows\Temp\vmware-tools.tar')
 (New-Object System.Net.WebClient).DownloadFile('https://packages.vmware.com/tools/esx/latest/windows/VMware-tools-windows-10.3.5-10430147.iso', 'C:\Windows\Temp\vmware-tools.iso')
-
 
 Write-Host "Mountain vmware tools ISO...."
 $mountResult = Mount-DiskImage -ImagePath C:\Windows\Temp\vmware-tools.iso -PassThru
@@ -14,7 +11,8 @@ $driveLetter = ($mountResult | Get-Volume).DriveLetter
 
 Write-Host "Running vmware tools setup/install...."
 $vmwareToolsSetupExe = $driveLetter + ':\' + "setup64.exe"
-cmd /c $vmwareToolsSetupExe /S /v"/qn REBOOT=R\"
+cmd /c "$vmwareToolsSetupExe /s /l C:\\Windows\\Temp\\vmware_tools.log /v`"/qn REBOOT=R`""
+
 
 Write-Host "Cleaning up vmware tools install...."
 Dismount-DiskImage -ImagePath C:\Windows\Temp\vmware-tools.iso
