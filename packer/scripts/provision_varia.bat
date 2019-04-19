53af684dfd883f924287209f15a0892eca110c58
rollwagen@gmail.com <gogs@fake.local>
2019-04-19 08:25:44 +0200

    Added reference link.

--- packer/scripts/provision_varia.bat
+++ packer/scripts/provision_varia.bat
@@ -12,7 +12,7 @@ sc config winrm start=auto
 echo Enable UAC
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /f /v EnableLUA /t REG_DWORD /d 1
 
-echo Disable Updates
+echo Disable Updates - see https://www.windows-commandline.com/disable-automatic-updates-command-line/
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" /v AUOptions /t REG_DWORD /d 1 /f
 reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" /f
 sc config wuauserv start=disabled
