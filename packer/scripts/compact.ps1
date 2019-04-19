0c6d4f2c5203ed9172fa0008aee1f4c21a917998
rollwagen@gmail.com <gogs@fake.local>
2019-04-19 21:38:40 +0200

    Fixes and change from admin user to 'vagrant'.

--- packer/scripts/compact.ps1
+++ packer/scripts/compact.ps1
@@ -3,7 +3,7 @@ Write-Host "Enabling >TLS1.0"
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 Write-Host "Downloading ultradefrag tool..."
-(New-Object System.Net.WebClient).DownloadFile('https://downloads.sourceforge.net/project/ultradefrag/stable-release/6.1.0/ultradefrag-portable-6.1.0.bin.amd64.zip', 'C:\Windows\Temp\ultradefrag.zip')
+(New-Object System.Net.WebClient).DownloadFile('https://sourceforge.net/projects/ultradefrag/files/stable-release/7.1.1/ultradefrag-portable-7.1.1.bin.amd64.zip', 'C:\Windows\Temp\ultradefrag.zip')
 
 Write-Host "Unzipping ultradefrag tool...."
 Expand-Archive C:\Windows\Temp\ultradefrag.zip C:\Windows\Temp
@@ -22,8 +22,8 @@ New-Item C:\Windows\SoftwareDistribution\Download
 # Start-Service wuauserv
 
 Write-Host "Running ultradefrag..."
-#cmd /c "C:\Windows\Temp\ultradefrag-portable-6.1.0.amd64\udefrag.exe --optimize --repeat C:"
-Start-Process -FilePath 'C:\Windows\Temp\ultradefrag-portable-6.1.0.amd64\udefrag.exe' -ArgumentList '--optimize --repeat C:' -wait
+# --optimize vs --quick-optimization
+Start-Process -FilePath 'C:\Windows\Temp\ultradefrag-portable-7.1.1.amd64\udefrag.exe' -ArgumentList '--quick-optimization --repeat C:' -wait
   
 
 Write-Host "Running sdelete..."
