3cfe21de774e343e7f71dd01630f8d0613ec182f
rollwagen@gmail.com <gogs@fake.local>
2019-04-21 13:36:05 +0200

    Initial commit.

---
+++ packer/scripts/disable-defender.ps1
@@ -0,0 +1,4 @@
+
+
+Set-MpPreference -DisableIntrusionPreventionSystem $true -DisableIOAVProtection $true -DisableRealtimeMonitoring $true -DisableScriptScanning $true -Force -MAPSReporting Disabled -SubmitSamplesConsent Never
+
