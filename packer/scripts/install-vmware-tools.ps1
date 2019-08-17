804c332cc4e5f38f4547582cee3b6a5380a47b12
rollwagen@gmail.com <rollwagen@gmail.com>
2019-08-17 18:30:33 +0200

    Installing Fusion specific VMWare tools now insted of general ESX one.

--- packer/scripts/install-vmware-tools.ps1
+++ packer/scripts/install-vmware-tools.ps1
@@ -3,10 +3,18 @@ Write-Host "Enabling >TLS1.0 for iso download over https..."
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 Write-Host "Downloading vmware tools..."
-(New-Object System.Net.WebClient).DownloadFile('https://packages.vmware.com/tools/esx/latest/windows/VMware-tools-windows-10.3.5-10430147.iso', 'C:\Windows\Temp\vmware-tools.iso')
+(New-Object System.Net.WebClient).DownloadFile('https://softwareupdate.vmware.com/cds/vmw-desktop/fusion/11.1.1/14328561/packages/com.vmware.fusion.tools.windows.zip.tar', 'C:\Windows\Temp\vmware-tools.zip.tar')
+
+# Extracts two files
+# x com.vmware.fusion.tools.windows.zip
+# x descriptor.xml
+cmd /c "tar -xvzf C:\Windows\Temp\vmware-tools.zip.tar -C C:\Windows\Temp\"
+
+# Extract C:\Windows\Temp\com.vmware.fusion.tools.windows.zip
+Expand-Archive C:\Windows\Temp\com.vmware.fusion.tools.windows.zip -DestinationPath C:\Windows\Temp\ -Force
 
 Write-Host "Mountain vmware tools ISO...."
-$mountResult = Mount-DiskImage -ImagePath C:\Windows\Temp\vmware-tools.iso -PassThru
+$mountResult = Mount-DiskImage -ImagePath C:\Windows\Temp\payload\windows.iso -PassThru
 $driveLetter = ($mountResult | Get-Volume).DriveLetter
 
 Write-Host "Running vmware tools setup/install...."
@@ -14,5 +22,5 @@ $vmwareToolsSetupExe = $driveLetter + ':\' + "setup64.exe"
 cmd /c "$vmwareToolsSetupExe /s /l C:\\Windows\\Temp\\vmware_tools.log /v`"/qn REBOOT=R`""
 
 #Write-Host "Cleaning up vmware tools install...."
-Dismount-DiskImage -ImagePath C:\Windows\Temp\vmware-tools.iso
-Remove-Item  C:\Windows\Temp\vmware-tools.iso
+Dismount-DiskImage -ImagePath C:\Windows\Temp\payload\windows.iso
+Remove-Item  C:\Windows\Temp\vmware-tools.zip.tar
