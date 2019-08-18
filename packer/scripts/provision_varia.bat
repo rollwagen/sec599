9f496c247bf2d9232dcc5c9b0b81c620b47f603e
rollwagen@gmail.com <rollwagen@gmail.com>
2019-08-18 10:29:27 +0200

    Added info to manual re-enable patching.

--- packer/scripts/provision_varia.bat
+++ packer/scripts/provision_varia.bat
@@ -1,9 +1,9 @@
 
-#        "./scripts/pin-powershell.bat",
-        "./scripts/set-winrm-automatic.bat",
-        "./scripts/uac-enable.bat",
-        "./scripts/dis-updates.bat",
-        "./scripts/compact.bat"
+rem        "./scripts/pin-powershell.bat",
+rem        "./scripts/set-winrm-automatic.bat",
+rem       "./scripts/uac-enable.bat",
+rem        "./scripts/dis-updates.bat",
+rem        "./scripts/compact.bat"
 
 
 echo Ensure WinRM start type is set to auto
@@ -14,8 +14,10 @@ reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /f /v E
 
 echo Disable Updates - see https://www.windows-commandline.com/disable-automatic-updates-command-line/
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" /v AUOptions /t REG_DWORD /d 1 /f
-reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" /f
+rem reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" /f
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /v NoAutoUpdate /d 1 /t REG_DWORD /f
 sc config wuauserv start=disabled
-net stop wuauserv
+rem net stop wuauserv
 
+rem Manual perform update:  net stop wuauserv; ren c:\windows\SoftwareDistribution softwaredistribution.old; net start wuauserv
 
