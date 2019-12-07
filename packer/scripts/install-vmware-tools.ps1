15651009768ce9b173e0e9faa1ecdfbec364b1ca
rollwagen@gmail.com <rollwagen@gmail.com>
2019-12-07 16:22:09 +0100

    Minor fixed/updates.

--- packer/scripts/install-vmware-tools.ps1
+++ packer/scripts/install-vmware-tools.ps1
@@ -3,7 +3,7 @@ Write-Host "Enabling >TLS1.0 for iso download over https..."
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 Write-Host "Downloading vmware tools..."
-(New-Object System.Net.WebClient).DownloadFile('https://softwareupdate.vmware.com/cds/vmw-desktop/fusion/11.1.1/14328561/packages/com.vmware.fusion.tools.windows.zip.tar', 'C:\Windows\Temp\vmware-tools.zip.tar')
+(New-Object System.Net.WebClient).DownloadFile('https://softwareupdate.vmware.com/cds/vmw-desktop/fusion/11.1.0/13668589/packages/com.vmware.fusion.tools.windows.zip.tar', 'C:\Windows\Temp\vmware-tools.zip.tar')
 
 # Extracts two files
 # x com.vmware.fusion.tools.windows.zip
@@ -21,6 +21,6 @@ Write-Host "Running vmware tools setup/install...."
 $vmwareToolsSetupExe = $driveLetter + ':\' + "setup64.exe"
 cmd /c "$vmwareToolsSetupExe /s /l C:\\Windows\\Temp\\vmware_tools.log /v`"/qn REBOOT=R`""
 
-#Write-Host "Cleaning up vmware tools install...."
+Write-Host "Cleaning up vmware tools install...."
 Dismount-DiskImage -ImagePath C:\Windows\Temp\payload\windows.iso
 Remove-Item  C:\Windows\Temp\vmware-tools.zip.tar
