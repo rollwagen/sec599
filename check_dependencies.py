504a511b1a3e69a4ed56a426cd16b1de9d745a7e
rollwagen <rollwagen@gmail.com>
2020-10-22 21:21:35 +0200

    Run 'black' on check_dependencies.py.

--- check_dependencies.py
+++ check_dependencies.py
@@ -4,13 +4,15 @@ from __future__ import print_function
 import subprocess
 import sys
 
+
 class output:
-    HEADER = '\033[95m'
-    BOLD = '\033[1m'
-    OK = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
+    HEADER = "\033[95m"
+    BOLD = "\033[1m"
+    OK = "\033[92m"
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+
 
 # Start checking pre-requisites
 print()
@@ -18,27 +20,27 @@ print()
 #
 # VMWare fusion
 #
-print(output.BOLD,"Checking for VMWare Fusion presence......", end='')
+print(output.BOLD, "Checking for VMWare Fusion presence......", end="")
 vmwarestatus = subprocess.call(["vmrun", "list"], stdout=subprocess.PIPE)
-if vmwarestatus !=0:
-	print(ouput.FAIL,"FAILED")
+if vmwarestatus != 0:
+    print(ouput.FAIL, "FAILED")
 else:
-	print(output.OK,"OK")
+    print(output.OK, "OK")
 
-print(output.ENDC, end='')
+print(output.ENDC, end="")
 
 
 #
 # Vagrant
 #
-print(output.BOLD, "Checking for Vagrant presence......", end='')
+print(output.BOLD, "Checking for Vagrant presence......", end="")
 sys.stdout.flush()
 vagrantstatus = subprocess.call(["vagrant", "status"], stdout=subprocess.PIPE)
-if vagrantstatus !=0:
-	print(output.FAIL,"FAILED")
+if vagrantstatus != 0:
+    print(output.FAIL, "FAILED")
 else:
-	print(output.OK,"OK")
+    print(output.OK, "OK")
 
 
 # End checking pre-requisites
-print(output.ENDC, end='')
+print(output.ENDC, end="")
