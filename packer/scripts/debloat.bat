547446d0c9d88d572e7b8f07edc088693bfcd7cc
rollwagen <rollwagen@gmail.com>
2020-11-15 18:09:21 +0100

    Initial commit.

---
+++ packer/scripts/debloat.bat
@@ -0,0 +1,40 @@
+
+@rem **** Remove AppxPackage apps ****
+PowerShell -Command "Get-AppxPackage *3DBuilder* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *Cortana* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *Getstarted* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *WindowsAlarms* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *WindowsCamera* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *bing* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *MicrosoftOfficeHub* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *OneNote* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *WindowsPhone* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *photos* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *SkypeApp* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *solit* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *WindowsSoundRecorder* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *windowscommunicationsapps* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *zune* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *WindowsMaps* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *Sway* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *CommsPhone* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *ConnectivityStore* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *Microsoft.Messaging* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *Microsoft.WindowsStore* | Remove-AppxPackage"
+
+rem **** Uninstall OneDrive ****
+start /wait "" "%SYSTEMROOT%\SYSWOW64\ONEDRIVESETUP.EXE" /UNINSTALL
+rd C:\OneDriveTemp /Q /S >NUL 2>&1
+rd "%USERPROFILE%\OneDrive" /Q /S >NUL 2>&1
+rd "%LOCALAPPDATA%\Microsoft\OneDrive" /Q /S >NUL 2>&1
+rd "%PROGRAMDATA%\Microsoft OneDrive" /Q /S >NUL 2>&1
+reg add "HKEY_CLASSES_ROOT\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}\ShellFolder" /f /v Attributes /t REG_DWORD /d 0 >NUL 2>&1
+reg add "HKEY_CLASSES_ROOT\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}\ShellFolder" /f /v Attributes /t REG_DWORD /d 0 >NUL 2>&1
+echo OneDrive was removed. Windows Explorer should be restarted.
+
+rem *** Show hidden files in Explorer ***
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "Hidden" /t REG_DWORD /d 1 /f
+rem *** Show (super) hidden system files in Explorer ***
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "ShowSuperHidden" /t REG_DWORD /d 1 /f
+rem *** Show file extensions in Explorer ***
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "HideFileExt" /t  REG_DWORD /d 0 /f
